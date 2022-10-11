d04c15946357883fa6c13457f56bc753b00cd82b
vivid20221 <julainj9011@gmail.com>
2022-10-10 21:59:55 -0500

    javaweather

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,18 @@
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 const citySearch = document.getElementById('search-city');
 const searchBtn = document.getElementById('search');
-const apiKey = "d311c6fd7ebc4183cb52d300bdd43ba2";
+const apiKey = "d8fb84e836945d223bbf58e5c2d3e94e";
 const currentTemp = document.getElementById('currentTemp');
 const currentWind = document.getElementById('currentWind');
 const currentHumidity = document.getElementById('currentHumidity');
+const monCurrentTemp = document.getElementById('monCurrentTemp');
+const monCurrentWind = document.getElementById('monCurrentWind');
+const monCurrentHumidity = document.getElementById('monCurrentHumidity')
+const 
 // removeItem(keyName)
-
 function getLatLon() {
-    var cityName = citySearch.value;
-    const cityApi = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    var cityName = citySearch.value; 
+    const cityApi = 'http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}';
     console.log(cityName);
     fetch(cityApi)
         .then(function(response) {
@@ -36,9 +39,11 @@ function secondCall(lat, lon) {
             console.log(data);
             currentTemp.textContent = data.list[0].main.temp;
             currentWind.textContent = data.list[0].wind.speed;
-            currentHumidity.textContent = data.list[0].main.humidity;
+           currentHumidity
         })
 }
+
+
 
 
 searchBtn.addEventListener('click', getLatLon);
